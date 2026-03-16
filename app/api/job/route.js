cfcfba40cb383f6f140214da615b49d390f84583
Ayush8481Lab <wonder945177@gmail.com>
2026-03-16 15:06:55 +0530

    Update route.js

--- app/api/job/route.js
+++ app/api/job/route.js
@@ -1,12 +1,14 @@
-import { kv } from '@vercel/kv';
+import { Redis } from '@upstash/redis';
 import { NextResponse } from 'next/server';
+
+const redis = Redis.fromEnv();
 
 export async function POST(request) {
     const { images, total } = await request.json();
     const jobId = Math.random().toString(36).substring(2, 10);
     
     // Create Job, Expires in 86400 seconds (24 Hours)
-    await kv.set(`job:${jobId}`, {
+    await redis.set(`job:${jobId}`, {
         status: "processing",
         total: total,
         completed: 0,
